ccc37a5bc03acbf71f16949685dd3a766d0ef380
ytewdgujhvdss-cyber <ytewdgujhvdss@gmail.com>
2026-06-25 18:52:07 +0800

    Bind training QA server to 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const rootDir = resolve(__dirname);
 await loadLocalEnv();
 
 const port = Number(process.env.PORT || 3000);
-const host = process.env.HOST || "127.0.0.1";
+const host = process.env.HOST || "0.0.0.0";
 
 const demoKnowledge = [
   {
